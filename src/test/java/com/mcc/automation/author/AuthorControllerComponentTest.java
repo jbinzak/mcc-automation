856b29beb831612fa5b07706f9d20651f39e1593
John Binzak <john.binzak@gmail.com>
2023-03-01 19:04:27 -0600

    author component tests

---
+++ src/test/java/com/mcc/automation/author/AuthorControllerComponentTest.java
@@ -0,0 +1,29 @@
+package com.mcc.automation.author;
+
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.http.MediaType;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
+
+import static org.hamcrest.Matchers.equalTo;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+@SpringBootTest
+@AutoConfigureMockMvc
+public class AuthorControllerComponentTest {
+
+
+	@Autowired
+	private AuthorController controller;
+
+	@Test
+	public void authorControllerContextLoads() throws Exception {
+		Assertions.assertThat(controller).isNotNull();
+	}
+
+}
